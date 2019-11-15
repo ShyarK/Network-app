4e4640dc8c7900f43bb0ff9770ec2e8845d78e88
ShyarK <shyr.khalil@gmail.com>
2019-11-15 14:55:42 +0100

    Prepared for deployment

--- server.js
+++ server.js
@@ -21,9 +21,9 @@ app.use('/api/auth', require('./routes/api/auth'));
 // Serve static assets in the production.
 if (process.env.NODE_ENV === 'production') {
   //set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static('client-app/build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client-app', 'build', 'index.html'));
   });
 }
 
